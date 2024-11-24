bc814da7e513efe5569e6e93c8a7d506f094e3a0
luksinocencio <luksinocencio@gmail.com>
2024-11-24 17:38:34 -0300

    *

--- src/components/forecast/ForecastCapsule.tsx
+++ src/components/forecast/ForecastCapsule.tsx
@@ -1,6 +1,6 @@
 import { Canvas, RoundedRect, Shadow } from "@shopify/react-native-skia";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { Forecast } from "../../models/Weather";
 
 interface ForecastCapsuleProps {
@@ -16,7 +16,9 @@ export function ForecastCapsule({
   height,
   radius,
 }: ForecastCapsuleProps) {
+  const { date, icon, probability, temperature } = forecast;
   const myStyles = styles({ width, height });
+
   return (
     <View style={myStyles.container}>
       <Canvas style={{ flex: 1 }}>
@@ -31,6 +33,14 @@ export function ForecastCapsule({
           <Shadow dx={5} dy={4} blur={10} color={"rgba(0,0,0,0.25)"} />
         </RoundedRect>
       </Canvas>
+      <View>
+        <Text>{date.toString()}</Text>
+        <View>
+          <Image source={icon} style={myStyles.image} />
+          <Text style={myStyles.probality}>{probability}</Text>
+        </View>
+        <Text style={myStyles.temperature}>{temperature}</Text>
+      </View>
     </View>
   );
 }
@@ -40,5 +50,29 @@ const styles = ({ width, height }: { width: number; height: number }) =>
     container: {
       height,
       width,
+    },
+    image: {
+      width: width / 2,
+      height: width / 2,
+    },
+    time: {
+      fontFamily: "SF-Semibold",
+      fontSize: 15,
+      lineHeight: 20,
+      color: "white",
+      letterSpacing: -0.5,
+    },
+    probality: {
+      fontFamily: "SF-Semibold",
+      fontSize: 13,
+      lineHeight: 18,
+      color: "#40cbd8",
+    },
+    temperature: {
+      fontFamily: "SF-Regular",
+      fontSize: 20,
+      lineHeight: 24,
+      color: "white",
+      letterSpacing: 0.38,
     },
   });
